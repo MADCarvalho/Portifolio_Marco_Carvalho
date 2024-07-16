18392bda80c5708a4fd88e1642a2a9d050fdd6f2
MADCarvalho <marco.delfino@hotmail.com>
2024-07-15 23:58:21 -0300

    first commit

---
+++ assets/js/api.js
@@ -0,0 +1,6 @@
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/paulorfsantos17/portifolio-dio-js/main/data/profile.json'
+    const fetching = await fetch(url)
+    
+    return await fetching.json()
+  }
